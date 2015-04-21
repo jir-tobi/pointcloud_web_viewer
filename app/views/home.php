d2875d4a3248b81570bc45039549722e3cd6ae74
root <root@tobi-robotics>
2015-04-21 15:25:15 +0200

    some beautification

--- app/views/home.php
+++ app/views/home.php
@@ -24,7 +24,7 @@
       <h1 style="font-size:40px;">Jacobs Robotics Object Database</h1><br />
       <p class="lead">Point clouds, mesh representations and Gazebo models of everyday objects.</p>
       
-      The object point clouds available on this website were created from low-cost RGBD sensor data without user interaction, see our <a href="http://http://dx.doi.org/10.1016/j.robot.2015.01.005" target="_blank">publication</a>: <i>Mihalyi et al. (Robotics and Autonomous Systems, April 2015): Robust 3D Object Modeling with a Low-Cost RGBD-Sensor and AR-Markers for Applications with Untrained End-Users</i>.
+      The object point clouds available on this website were created from low-cost RGBD sensor data without user interaction, see our <a href="http://dx.doi.org/10.1016/j.robot.2015.01.005" target="_blank">publication</a>: <i>Mihalyi et al. (Robotics and Autonomous Systems, April 2015): Robust 3D Object Modeling with a Low-Cost RGBD-Sensor and AR-Markers for Applications with Untrained End-Users</i>.
       <br><br>
       From these point clouds, watertight mesh representations were generated as described in <i>Fromm et al. (submitted to IROS 2015): Unsupervised Watertight Mesh Generation From Noisy Free-Formed RGBD Object Models Using Growing Neural Gas</i>.
       <hr>
@@ -32,6 +32,7 @@
       <ul>
           <li>PCD point cloud</li>
           <li>PLY mesh</li>
+          <li>PNG preview image</li>
           <li>Gazebo model folder</li>
       </ul>
     </div>
@@ -87,16 +88,17 @@
           $dataSize = round(filesize($dataFile) / (1000000));
 
           ?>
-          <div class="col-lg-4 col-sm-4 col-xs-6">
+          <div class="col-lg-3 col-sm-4 col-xs-6">
             <div class="thumbnail">
               <div class="caption">
-                <h2><?php echo $title ?></h2>
-                <div style="text-align:left; padding:10px;">
-                  <h4><?php echo $desc ?></h4>
-                  <h5>Point Cloud Size: <?php echo $pcSize ?>MB, ZIP File Size: <?php echo $dataSize ?>MB</h5>
+                <h4><?php echo $title ?></h4>
+                <div style="text-align:left; padding:5px;">
+                  <h5><?php echo $desc ?></h5>
+                  <h5>Point Cloud Size: <?php echo $pcSize ?>MB<br>
+                  ZIP File Size: <?php echo $dataSize ?>MB</h5>
                 </div>
-                <p><a class="btn btn-lg btn-primary" href="view/<?php echo $folderName ?>">View</a>
-                <a class="btn btn-lg btn-primary" href="<?php echo $dataFile ?>">Download</a></p>
+                <p><a class="btn btn-sm btn-primary" href="view/<?php echo $folderName ?>">View</a>
+                <a class="btn btn-sm btn-success" href="<?php echo $dataFile ?>">Download</a></p>
               </div>
               <img class="img-responsive" src="<?php echo $imgFile ?>">
             </div>
