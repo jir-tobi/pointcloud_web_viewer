73316004149b72cae95bca98ccd8713c02b6b1c3
root <root@robotics.jacobs-university.de>
2015-09-14 19:04:53 +0200

    added show/hide explanations button

--- app/views/home.php
+++ app/views/home.php
@@ -19,6 +19,12 @@
 	      $(this).parent().next().collapse('toggle');
 	  });
 
+      $("#buttonShow").click(function() {
+	    $("#intro").toggleClass('hide');
+	    $("#introul").toggleClass('hide');
+            $(this).toggleClass('active');
+      });
+
   });
   
   </script>
@@ -29,20 +35,26 @@
     <div class="col-lg-8">
       <h1 style="font-size:40px;">Jacobs Robotics Object Database</h1><br />
       <p class="lead">Point clouds, mesh representations and Gazebo models of everyday objects.</p>
-      
+
+      <button type="button" class="btn btn-primary btn-lg" id="buttonShow">Show detailed explanation</button>
+
+      <p class="text hide" id="intro">
+      <br>
       The object point clouds available on this website were created from low-cost RGBD sensor data without user interaction, see our <a href="http://dx.doi.org/10.1016/j.robot.2015.01.005" target="_blank">publication</a>: <i>Mihalyi et al. (Robotics and Autonomous Systems, April 2015): Robust 3D Object Modeling with a Low-Cost RGBD-Sensor and AR-Markers for Applications with Untrained End-Users</i>.
       <br><br>
       From these point clouds, watertight mesh representations were generated as described in <i>Fromm et al. (submitted to ICRA 2016): Unsupervised Watertight Mesh Generation From Noisy Free-Form RGBD Object Models Using Growing Neural Gas</i>.
-      <hr>
+      <br><br>
       Each object's point cloud and mesh can be viewed live. We provide zipped .bag files for the objects which were used in our modeling approach as well as .zip files containing:
-      <ul>
+
+      <ul class="text hide" id="introul">
           <li>PCD point cloud</li>
           <li>PLY mesh</li>
           <li>PNG preview image</li>
           <li>Gazebo model folder</li>
       </ul>
+      </p>  
     </div>
-
+    
     <div class="col-lg-2 col-lg-offset-2">
       <div style="width:100%; height: 20px;"></div>
       <a title="Jacobs Robotics" href="http://robotics.jacobs-university.de"><img class="img-responsive" src="img/logo.png" width="250"></a>
